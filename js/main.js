d31e298c7affb0704532ee49f98fd50fed765b9c
Pham Duy Tien <tienpd901@gmail.com>
2015-12-26 15:25:18 +0700

    update right nav

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+//Set height for slideshow and dropdown right menu.
+window.onload = function() {
+	var h = window.innerHeight;
+	var slide_h = h - 230;
+	$('.bx-viewport').css('height', slide_h + 'px');
+
+	var dropdown_h = h - 93;
+
+	$('.db-list-nav-items').css('height', dropdown_h + 'px');
+};
+
 $('#btn-toggle-nav').click(function(){
 	$('.db-list-nav-items').slideToggle();
 });
@@ -5,3 +16,4 @@ $('#btn-toggle-nav').click(function(){
 $('#btn-show-input').click(function(){
 	$('.db-form-subcribe').toggle("slide",1000);
 });
+
